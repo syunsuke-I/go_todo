6c381a03ede45f35ac131f524035bc64a4ad7118
syunsuke-I <odol@ymail.ne.jp>
2023-11-22 14:00:57 +0900

    todoの取得

--- main.go
+++ main.go
@@ -7,14 +7,6 @@ import (
 
 func main() {
 
-	fmt.Println(models.Db)
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@test"
-	u.Password = "testpassword"
-
-	u.CreateUser()
-
-	user, _ := models.GetUser(2)
-	user.CreateTodo("first Todo")
+	todo, _ := models.GetTodo(1)
+	fmt.Println(todo)
 }
