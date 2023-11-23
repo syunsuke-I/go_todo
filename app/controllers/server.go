e17beb14a0f5f407e471d430ebd2e56f3344d8c7
syunsuke-I <odol@ymail.ne.jp>
2023-11-23 14:46:29 +0900

    レイアウトの共通化

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
+	"fmt"
 	"lesson/config"
 	"net/http"
+	"text/template"
 )
+
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+	template := template.Must(template.ParseFiles(files...))
+	template.ExecuteTemplate(w, "layout", data)
+}
 
 func StartMainSever() error {
 	http.HandleFunc("/", top)
